0a12b7e442a972cf5d96df0e37a3c613311485fe
name <varunbrahmane30@gmail.com>
2021-07-23 17:04:33 +0530

    [varun] added: maximum in String method calling

--- Program.cs
+++ Program.cs
@@ -9,6 +9,13 @@ namespace Day13_FindMaximumNumber_Generics_
             Console.WriteLine("Find Maximum Number");
             int intMaxNumber = CheckMaximum.MaximumIntegerNumber(55, 24, 100);
             Console.WriteLine("Maximum number of INT : "+ intMaxNumber);
+
+            float floatMaxNumber = CheckMaximum.MaximumFloatNumber(5.5f, 2.4f, 1.0f);
+            Console.WriteLine("Maximum number of Float : " + floatMaxNumber);
+
+            string stringMaxNumber = CheckMaximum.MaximumStringNumber("Apple", "Peach", "Banana");
+            Console.WriteLine("Maximum number of String : " + stringMaxNumber);
+
         }
     }
 }
