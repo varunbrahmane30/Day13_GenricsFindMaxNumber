148a58a925abdcca145542e58ec6789332bd24c0
name <varunbrahmane30@gmail.com>
2021-07-23 16:53:43 +0530

    [varun] added: maximum float number method calling

--- Program.cs
+++ Program.cs
@@ -9,6 +9,9 @@ namespace Day13_FindMaximumNumber_Generics_
             Console.WriteLine("Find Maximum Number");
             int intMaxNumber = CheckMaximum.MaximumIntegerNumber(55, 24, 100);
             Console.WriteLine("Maximum number of INT : "+ intMaxNumber);
+
+            float floatMaxNumber = CheckMaximum.MaximumFloatNumber(5.5f, 2.4f, 1.0f);
+            Console.WriteLine("Maximum number of INT : " + floatMaxNumber);
         }
     }
 }
