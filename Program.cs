5df81dcb6f599a461de8bb78142a7a88f1371284
name <varunbrahmane30@gmail.com>
2021-07-23 17:25:38 +0530

    [varun] added: calling the print method

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Day13_FindMaximumNumber_Generics_
         static void Main(string[] args)
         {
             Console.WriteLine("Find Maximum Number");
+            #region  Using normal method 
             //int intMaxNumber = CheckMaximum.MaximumIntegerNumber(55, 24, 100);
             //Console.WriteLine("Maximum number of INT : "+ intMaxNumber);
 
@@ -15,16 +16,21 @@ namespace Day13_FindMaximumNumber_Generics_
 
             //string stringMaxNumber = CheckMaximum.MaximumStringNumber("Apple", "Peach", "Banana");
             //Console.WriteLine("Maximum number of String : " + stringMaxNumber);
+            #endregion
 
+            #region   Using Generics 
             int[] intArray = { 11, 3442, 4032, 55, 68 };
             UsingGenericsMaxCount<int> intMaxNumber = new UsingGenericsMaxCount<int>(intArray);
+            intMaxNumber.PrintMaxValue();
 
             float[] floatArray = { 1.2f, 340.4f, 44.36f, 51.55f, 67.78f };
             UsingGenericsMaxCount<float> floatMaxNumber = new UsingGenericsMaxCount<float>(floatArray);
+            floatMaxNumber.PrintMaxValue();
 
-            string[] stringArray = { "apple", "mango", "grapes", "banana", "pineapple" };
+            string[] stringArray = { "Apple", "Mango", "Grapes", "Banana", "Pineapple" };
             UsingGenericsMaxCount<string> stringMax = new UsingGenericsMaxCount<string>(stringArray);
-
+            stringMax.PrintMaxValue();
+            #endregion
         }
     }
 }
