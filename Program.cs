8c8930d7db63a583b589f9c3c2493b038aa4eff9
name <varunbrahmane30@gmail.com>
2021-07-23 17:18:54 +0530

    [varun] added: using class generics extended values to check

--- Program.cs
+++ Program.cs
@@ -7,14 +7,23 @@ namespace Day13_FindMaximumNumber_Generics_
         static void Main(string[] args)
         {
             Console.WriteLine("Find Maximum Number");
-            int intMaxNumber = CheckMaximum.MaximumIntegerNumber(55, 24, 100);
-            Console.WriteLine("Maximum number of INT : "+ intMaxNumber);
+            //int intMaxNumber = CheckMaximum.MaximumIntegerNumber(55, 24, 100);
+            //Console.WriteLine("Maximum number of INT : "+ intMaxNumber);
 
-            float floatMaxNumber = CheckMaximum.MaximumFloatNumber(5.5f, 2.4f, 1.0f);
-            Console.WriteLine("Maximum number of Float : " + floatMaxNumber);
+            //float floatMaxNumber = CheckMaximum.MaximumFloatNumber(5.5f, 2.4f, 1.0f);
+            //Console.WriteLine("Maximum number of Float : " + floatMaxNumber);
 
-            string stringMaxNumber = CheckMaximum.MaximumStringNumber("Apple", "Peach", "Banana");
-            Console.WriteLine("Maximum number of String : " + stringMaxNumber);
+            //string stringMaxNumber = CheckMaximum.MaximumStringNumber("Apple", "Peach", "Banana");
+            //Console.WriteLine("Maximum number of String : " + stringMaxNumber);
+
+            int[] intArray = { 11, 3442, 4032, 55, 68 };
+            UsingGenericsMaxCount<int> intMaxNumber = new UsingGenericsMaxCount<int>(intArray);
+
+            float[] floatArray = { 1.2f, 340.4f, 44.36f, 51.55f, 67.78f };
+            UsingGenericsMaxCount<float> floatMaxNumber = new UsingGenericsMaxCount<float>(floatArray);
+
+            string[] stringArray = { "apple", "mango", "grapes", "banana", "pineapple" };
+            UsingGenericsMaxCount<string> stringMax = new UsingGenericsMaxCount<string>(stringArray);
 
         }
     }
---
+++ UsingGenericsMaxCount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day13_FindMaximumNumber_Generics_
+{
+    class UsingGenericsMaxCount<T> where T : IComparable
+    {
+        public T[] value;
+        public UsingGenericsMaxCount(T[] value)
+        {
+            this.value = value;
+        }
+
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+
+        }
+
+
+        public T MaxValue(params T[] values)
+        {
+            var sorted_values = Sort(values);
+
+            return sorted_values[sorted_values.Length - 1];
+        }
+
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.value);
+            return max;
+        }
+    }
+}
